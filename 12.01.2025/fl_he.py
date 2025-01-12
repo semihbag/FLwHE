dbf89240f8b97d4d047128141a26381f39fc2072
semihbag <semih702bag@gmail.com>
2025-01-12 16:41:11 +0300

    derleme hatası he için

--- 12.01.2025/fl_he.py
+++ 12.01.2025/fl_he.py
@@ -22,6 +22,7 @@ def create_cnn_model():
         layers.Dense(128, activation='relu'),
         layers.Dense(10, activation='softmax')
     ])
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     return model
 
 # Homomorphic Encryption Setup
